e006808e6dc12bc062d467eafc482173beaef5e4
alexb <alexbondar03@gmail.com>
2022-11-04 22:54:01 +0300

    Add speakers model

--- src/main/java/by/tc/task01/entity/Speakers.java
+++ src/main/java/by/tc/task01/entity/Speakers.java
@@ -1,5 +1,21 @@
 package by.tc.task01.entity;
 
+import by.tc.task01.entity.criteria.SearchCriteria;
+
 public class Speakers extends Appliance{
-	// TODO you may add your own code here
+
+    public int powerConsumption;
+    public int numberOfSpeakers;
+    public String frequencyRange;
+    public int cordLength;
+
+    @Override
+    public boolean matches(String key, Object value) {
+        return switch (SearchCriteria.Speakers.valueOf(key)){
+            case POWER_CONSUMPTION -> powerConsumption == (Integer) value;
+            case NUMBER_OF_SPEAKERS -> numberOfSpeakers ==  (Integer) value;
+            case FREQUENCY_RANGE -> frequencyRange.equals(value);
+            case CORD_LENGTH -> cordLength == (Integer) value;
+        };
+    }
 }
