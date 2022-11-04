630793169e9dc3bb7eb1f7fd23a8318a1893ec15
alexb <alexbondar03@gmail.com>
2022-11-04 22:56:21 +0300

    Add tabletPC model

--- src/main/java/by/tc/task01/entity/TabletPC.java
+++ src/main/java/by/tc/task01/entity/TabletPC.java
@@ -1,5 +1,26 @@
 package by.tc.task01.entity;
 
+import by.tc.task01.entity.criteria.SearchCriteria;
+import by.tc.task01.entity.enums.PCColor;
+
+import javax.swing.plaf.synth.ColorType;
+
 public class TabletPC extends Appliance{
-	// TODO you may add your own code here
+
+    public int batteryCapacity;
+    public int displayInches;
+    public int memoryRom;
+    public int flashMemoryCapacity;
+    public PCColor color;
+
+    @Override
+    public boolean matches(String key, Object value) {
+        return switch (SearchCriteria.TabletPC.valueOf(key)){
+            case BATTERY_CAPACITY -> batteryCapacity == (Integer) value;
+            case DISPLAY_INCHES -> displayInches ==  (Integer) value;
+            case MEMORY_ROM -> memoryRom == (Integer) value;
+            case FLASH_MEMORY_CAPACITY -> flashMemoryCapacity == (Integer) value;
+            case COLOR -> color == PCColor.valueOf((String) value);
+        };
+    }
 }
