5b98e56435b58d08a4d03a49cc4ca67a950e86b9
alexb <alexbondar03@gmail.com>
2022-11-04 22:58:35 +0300

    Add cleaner model

--- src/main/java/by/tc/task01/entity/VacuumCleaner.java
+++ src/main/java/by/tc/task01/entity/VacuumCleaner.java
@@ -1,5 +1,26 @@
 package by.tc.task01.entity;
 
+import by.tc.task01.entity.criteria.SearchCriteria;
+import by.tc.task01.entity.enums.CleanerType;
+
 public class VacuumCleaner extends Appliance{
-	// TODO you may add your own code here
+
+    public int powerConsumption;
+    public CleanerType cleanerType;
+    public String bagType;
+    public String wandType;
+    public int motorSpeedRegulation;
+    public int cleaningWidth;
+
+    @Override
+    public boolean matches(String key, Object value) {
+        return switch (SearchCriteria.VacuumCleaner.valueOf(key)){
+            case POWER_CONSUMPTION -> powerConsumption == (Integer) value;
+            case FILTER_TYPE -> cleanerType == CleanerType.valueOf((String) value);
+            case BAG_TYPE -> bagType.equals(value);
+            case WAND_TYPE -> wandType.equals(value);
+            case MOTOR_SPEED_REGULATION -> motorSpeedRegulation == (Integer) value;
+            case CLEANING_WIDTH -> cleaningWidth == (Integer) value;
+        };
+    }
 }
---
+++ src/main/java/by/tc/task01/entity/enums/CleanerType.java
@@ -0,0 +1,8 @@
+package by.tc.task01.entity.enums;
+
+public enum CleanerType {
+    A1,
+    A2,
+    B1,
+    C1
+}
