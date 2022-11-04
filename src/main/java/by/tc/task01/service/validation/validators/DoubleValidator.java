3794b0481b466fcc8c4bd295e5c05861434c9ed6
alexb <alexbondar03@gmail.com>
2022-11-05 00:18:42 +0300

    Add validators

---
+++ src/main/java/by/tc/task01/service/validation/validators/DoubleValidator.java
@@ -0,0 +1,10 @@
+package by.tc.task01.service.validation.validators;
+
+import by.tc.task01.service.validation.ValueValidator;
+
+public class DoubleValidator implements ValueValidator {
+    @Override
+    public boolean validate(Object obj) {
+        return false;
+    }
+}
