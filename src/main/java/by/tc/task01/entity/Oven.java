5306840507c208c43d33c8fc53f8f9802f7e315e
alexb <alexbondar03@gmail.com>
2022-11-04 22:44:53 +0300

    Add oven model

--- src/main/java/by/tc/task01/entity/Oven.java
+++ src/main/java/by/tc/task01/entity/Oven.java
@@ -1,5 +1,25 @@
 package by.tc.task01.entity;
 
+import by.tc.task01.entity.criteria.SearchCriteria;
+
 public class Oven extends Appliance{
-	// TODO you may add your own code here
+
+    public int powerConsumption;
+    public int weight;
+    public int capacity;
+    public int depth;
+    public double height;
+    public double width;
+
+    @Override
+    public boolean matches(String key, Object value) {
+        return switch (SearchCriteria.Oven.valueOf(key)){
+            case POWER_CONSUMPTION -> powerConsumption == (Integer) value;
+            case WEIGHT -> weight ==  (Integer) value;
+            case CAPACITY -> capacity == (Integer) value;
+            case DEPTH -> depth == (Integer) value;
+            case HEIGHT -> height == Double.parseDouble(String.valueOf(value));
+            case WIDTH -> width == Double.parseDouble(String.valueOf(value));
+        };
+    }
 }
