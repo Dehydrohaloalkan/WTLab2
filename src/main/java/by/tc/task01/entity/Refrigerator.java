ac64dba9d67894429712c8e6f698fc3c0a8f384b
alexb <alexbondar03@gmail.com>
2022-11-04 22:49:56 +0300

    Add refrigerator model

--- src/main/java/by/tc/task01/entity/Refrigerator.java
+++ src/main/java/by/tc/task01/entity/Refrigerator.java
@@ -1,5 +1,25 @@
 package by.tc.task01.entity;
 
+import by.tc.task01.entity.criteria.SearchCriteria;
+
 public class Refrigerator extends Appliance{
-	// TODO you may add your own code here
+
+    public int powerConsumption;
+    public int weight;
+    public int freezerCapacity;
+    public double overallCapacity;
+    public double height;
+    public double width;
+
+    @Override
+    public boolean matches(String key, Object value) {
+        return switch (SearchCriteria.Refrigerator.valueOf(key)){
+            case POWER_CONSUMPTION -> powerConsumption == (Integer) value;
+            case WEIGHT -> weight ==  (Integer) value;
+            case FREEZER_CAPACITY -> freezerCapacity == (Integer) value;
+            case OVERALL_CAPACITY -> overallCapacity == Double.parseDouble(String.valueOf(value));
+            case HEIGHT -> height == Double.parseDouble(String.valueOf(value));
+            case WIDTH -> width == Double.parseDouble(String.valueOf(value));
+        };
+    }
 }
